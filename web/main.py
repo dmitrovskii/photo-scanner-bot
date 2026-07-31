fa5c1d4dc04999bccf4fa2744e4be4b1348ff09f
dmitrovskii <deniskrot99@gmail.com>
2026-07-31 15:18:45 +0300

    feat(web): ініціалізувати базовий веб-інтерфейс на NiceGUI
    - Створено точку входу web/main.py та базовий каркас сторінки (Header, Drawer).
    - Реалізовано ізольований компонент галереї (GalleryComponent) для відображення референсів.
    - Налаштовано асинхронне оновлення галереї без перезавантаження сторінки (через @ui.refreshable та ui.timer).
    - Створено requirments.txt.
    - Додано nicegui до списку залежностей проєкту.

---
+++ web/main.py
@@ -0,0 +1,49 @@
+from nicegui import ui
+
+from core.database import get_items
+
+def setup_layout():
+    with ui.header(elevated=True).classes('items-center justify-between'):
+        ui.label("HEADER")
+        
+    with ui.left_drawer().props('bordered'):
+        ui.label('LEFT DRAWER')
+
+class GalleryComponent:
+    def __init__(self) -> None:
+        with ui.dialog() as self.dialog, ui.card().classes('w-full max-w-1xl p-2'):
+            self.dialog_img = ui.image().classes('w-full')
+
+        self.last_file_count = 0 
+        
+        ui.timer(interval=5.0, callback=self.check_new_photos)
+
+    async def check_new_photos(self):
+        items = await get_items()
+
+        if len(items[0]) != self.last_file_count:
+            self.last_file_count = len(items[0])
+            self.render.refresh()
+
+    def open_photo(self, img_path):
+        self.dialog_img.set_source(img_path)
+        self.dialog.open()
+
+    @ui.refreshable
+    async def render(self):
+        with ui.row().classes('gap-1'):
+            items, next_offset = await get_items()
+            for item in items:
+                with ui.card().tight().classes('no-shadow'):
+                    ui.image(item["photo_path"]) \
+                        .classes('w-40 aspect-square cursor-pointer') \
+                        .on('click', lambda img=item["photo_path"]: self.open_photo(img))
+
+@ui.page("/")
+async def control_page():
+    setup_layout()
+
+    gallery = GalleryComponent()
+    await gallery.render() # type: ignore
+
+ui.run()
