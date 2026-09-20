e979a8028decc84d12ef299230001510ba811a11
dmitrovskii <deniskrot99@gmail.com>
2026-09-20 22:57:16 +0300

    refactor(web): очистити меню навігації та шапку інтерфейсу
    - Видалено пункт "Гайд" та закоментований словник "Тест" із MENU_ITEMS
    - Прибрано тестовий напис "HEADER" із шапки сторінки

--- web/components/header.py
+++ web/components/header.py
@@ -3,14 +3,11 @@ from nicegui import ui
 MENU_ITEMS = [
     {'label': 'Головна сторінка', 'icon': 'home', 'path': '/'},
     {'label': 'Додати зображення', 'icon': 'add_photo_alternate', 'path': '/image/create'},
-    {'label': 'Гайд', 'icon': 'lightbulb', 'path': '/questions'},
-    # {'label': 'ТЕСТ', 'icon': 'lightbulb', 'path': '/test'},
 ]
 
 def setup_layout():
     with ui.header(elevated=True).classes('items-center justify-start'):
         ui.button(on_click=lambda: left_drawer.toggle(), icon='menu').props('flat color=white')
-        ui.label("HEADER")
 
     with ui.left_drawer(fixed=False).classes('items-center p-2 space-y-1').props('bordered') as left_drawer:
         ui.label('Меню навігації').classes('text-xm font-bold text-gray-600')
